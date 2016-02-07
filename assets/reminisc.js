a08709a60607278c7b6ee1ab8aa545a53ec881e5
Thomas Quinn <quinntf.dev@gmail.com>
2016-02-07 01:23:22 -0500

    made the app

--- assets/reminisc.js
+++ assets/reminisc.js
@@ -1,16 +1,32 @@
-angular.module('Reminisc', [])
+angular.module('Reminisc', ['wu.masonry'])
 
-.controller('ReminiscCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
+.controller('ReminiscCtrl', ['$scope', '$http', '$location',  function($scope, $http, $location) {
 
     $scope.isLoggedIn = false;
-    $scope.checkLogin = function() {
-        $http.get('/api/isLoggedIn').success(function(data) {
-           $scope.isLoggedIn = (data == "true");
-        });
-    }
+    $scope.imgs = null;
+    $scope.loadComplete = false;
+    $scope.showResults = false;
 
     $scope.fbLogin = function() {
         $location.path('/api/auth');
     };
+
+    $scope.loadData = function() {
+        $http.get('/api/getPictures').success(function(data) {
+            $scope.imgs = (data == "null") ? null : data;
+            $scope.loadComplete = true;
+        });
+    }
+
+    $scope.checkLogin = function() {
+        $http.get('/api/isLoggedIn').success(function(data) {
+           $scope.isLoggedIn = (data == "true");
+           if ($scope.isLoggedIn) $scope.loadData();
+        });
+    }
+
+    $scope.startSearch = function() {
+        $scope.showResults = true;
+    }
 
 }]);
